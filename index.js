dc9885f1e12d58bccbb55ac9c5715c70ba91c438
AlaxDav1d <alaxdavid13@gmail.com>
2023-03-31 22:12:05 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,61 @@
+//if else
+//and(&&)
+//or(||)
+
+
+
+
+
+/*let temp = 37;
+
+if(temp >= 37.5){
+    console.log("Febre")
+}else if(temp == 37){
+    console.log("quase febre ")
+}
+else{
+    console.log("Nao esta com febre")
+}
+let pao = false;
+let queijo = true;
+if(pao || queijo){
+    console.log("como")
+}
+if(pao && queijo){
+    console.log("como")
+}
+switch('a'){
+    case 'a':
+        console.log("A");
+        break
+    case 'b':
+        console.log("B");
+        break
+    default:console.log("Erro");
+    break
+}
+function calc(n1,n2,op){
+    let result;
+    switch(op){
+        case'+':
+        result = n1 + n2;
+        break;
+        case'-':
+        result = n1 - n2;
+        break; case'*':
+        result = n1 * n2;
+        break;
+        case'/':
+        result = n1 / n2;
+        break;
+    }
+}
+//laços de repetição
+for(let i = 0;i <= 10;i++){
+    console.log(`2 X ${i} = `,2 * i); 
+}
+let teste = 0
+while(teste <= 10){
+    console.log(`3 X ${teste} = `,3 * teste);
+    teste++ 
+}*/
